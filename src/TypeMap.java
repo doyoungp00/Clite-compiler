4f1d598efd5569b694465857dca17f157bf61797
Doyoung Park <doyoungp.00@gmail.com>
2024-05-08 10:26:17 +0900

    Add type checking

---
+++ src/TypeMap.java
@@ -0,0 +1,8 @@
+import java.util.HashMap;
+
+public class TypeMap extends HashMap<Variable, Type> {
+
+// TypeMap is implemented as a Java HashMap.  
+// Plus a 'display' method to facilitate experimentation.
+
+}
